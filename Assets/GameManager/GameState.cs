a9f0331281ad65b696816f33efa3a0414bd7ccc0
Code Fox <mailtohadde@gmail.com>
2017-12-24 15:55:52 +0100

    inital commit

---
+++ Assets/GameManager/GameState.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameManager {
+
+    public class GameState : Countable {
+
+        private static GameState instance;
+
+        private GameManager gameManager = GameManager.getInstance();
+
+        private GameState() {
+            gameManager.registerHourNotify(getInstance());
+        }
+
+        public static GameState getInstance() {
+            if (instance == null) {
+                instance = new GameState();
+                Debug.Log("creating new Intance of GameState");
+            }
+            return instance;
+        }
+
+        private int currentHour = 1;
+        private int currentDay = 1;
+        private int currentGoldPrice;
+
+
+        public int getCurrentTime() {
+            return currentHour;
+        }
+
+        public override void oneHourPassed() {
+            currentHour = (currentHour + 1) % 24;
+            if (currentHour == 0) {
+                currentDay += 1;
+            }
+        }
+
+        public int[] getTime() {
+            int[] time = new int[2];
+            time[0] = currentDay;
+            time[1] = currentHour;
+            return time;
+        }
+
+        // Use this for initialization
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+    }
+}
---
+++ Assets/GameManager/UIManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace GameManager {
+
+    public class UIManager : MonoBehaviour {
+
+        private GameManager gameManager = GameManager.getInstance();
+
+        public Text timeObject;
+    
+    // Use this for initialization
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+            updateTime();
+        }
+
+        private void updateTime() {
+            int[] time = gameManager.getGameState().getTime();
+            timeObject.text = "Day: " + time[0] + " Hour: " + time[1];
+        }
+    }
+}
