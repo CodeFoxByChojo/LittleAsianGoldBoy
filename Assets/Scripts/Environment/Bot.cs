390cac55543ab666b91c2820555fa5148f56ed13
CodeFoxByChojo <playhadde@gmail.com>
2018-01-23 21:56:21 +0100

    created placeholder classes

---
+++ Assets/Scripts/Environment/Bot.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Countable;
+
+namespace Environment {
+    public class Bot : CountableClass {
+        public override void oneHourPassed() {
+            throw new NotImplementedException();
+        }
+
+        // Use this for initialization
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+    }
+}
