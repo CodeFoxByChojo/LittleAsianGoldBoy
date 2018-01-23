21d9fed73fc58a767260e81546ce5db60bfc4a8f
CodeFoxByChojo <playhadde@gmail.com>
2018-01-23 21:58:52 +0100

    Created Environment singleton

---
+++ Assets/Scripts/Environment/Environment.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Environment {
+    public class Environment : MonoBehaviour {
+        public static Environment instance;
+        private void Awake() {
+            if (instance == null) {
+                instance = this;
+            } else if (instance != this) {
+                Destroy(gameObject);
+            }
+            DontDestroyOnLoad(gameObject);
+        }
+
+
+        // Use this for initialization
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+
+        }
+    }
+}
