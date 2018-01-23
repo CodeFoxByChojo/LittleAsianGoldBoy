70ef9af94aae393c7e2b2c42966a781ae18af56e
CodeFoxByChojo <playhadde@gmail.com>
2018-01-23 21:57:12 +0100

    created ConfigHandler and DataHandler

---
+++ Assets/Scripts/DataHandler/ConfigHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DataHandler {
+    public class ConfigHandler : MonoBehaviour {
+
+        private ConfigData configData = new ConfigData();
+        private bool configLoaded = false;
+
+        protected internal ConfigData getConfigData() {
+            //loadConfigData();
+            return configData;
+        }
+        private void loadConfigData() {
+            if (configLoaded) return;
+            configData.setKnowledgePerHour(1);
+            //TODO Implement YAML Parser to get ConfigData
+        }
+    }
+}
