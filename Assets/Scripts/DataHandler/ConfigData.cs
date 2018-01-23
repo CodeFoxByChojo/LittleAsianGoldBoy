97224fbc18ed18d297361efb82be07974173bad5
CodeFoxByChojo <playhadde@gmail.com>
2018-01-23 21:56:39 +0100

    added first config data //Todo getter and setter

---
+++ Assets/Scripts/DataHandler/ConfigData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DataHandler {
+    public class ConfigData : MonoBehaviour {
+
+        private int knowledgePerHour = 1;
+        private int licenseCost = 1;
+        private float upgradeCostIncrease = 2.0f;
+        private int computerPrice = 100;
+        private int botLicensePrice = 50;
+        private int bandwidePrice = 200;
+        private int roomPrice = 100;
+        private int minMotherWaitDuration = 10;
+        private int maxMotherWaitDuration = 10;
+        private float timeCycleInSeconds = 2.0f;
+
+    }
+}
