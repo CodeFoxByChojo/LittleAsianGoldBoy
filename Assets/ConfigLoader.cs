6626e7203af73610789fca5c9aa5fe79140bbcec
Code Fox <playhadde@gmail.com>
2018-01-25 13:56:09 +0100

    removed Config loader for the monent

--- Assets/ConfigLoader.cs
+++ Assets/ConfigLoader.cs
@@ -1,91 +1,91 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using YamlDotNet.RepresentationModel;
-using YamlDotNet.Samples.Helpers;
-using UnityEngine;
-using System.IO;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using YamlDotNet.RepresentationModel;
+//using YamlDotNet.Samples.Helpers;
+//using UnityEngine;
+//using System.IO;
 
-public class ConfigLoader : MonoBehaviour {
+//public class ConfigLoader : MonoBehaviour {
 
-    private readonly ITestOutputHelper output;
+//    private readonly ITestOutputHelper output;
 
-    public ConfigLoader(ITestOutputHelper output) {
-        this.output = output;
-    }
+//    public ConfigLoader(ITestOutputHelper output) {
+//        this.output = output;
+//    }
 
-    public void Main() {
-        // Setup the input
-        var input = new StringReader(Document);
+//    public void Main() {
+//        // Setup the input
+//        var input = new StringReader(Document);
 
-        // Load the stream
-        var yaml = new YamlStream();
-        yaml.Load(input);
+//        // Load the stream
+//        var yaml = new YamlStream();
+//        yaml.Load(input);
 
-        // Examine the stream
-        var mapping =
-            (YamlMappingNode)yaml.Documents[0].RootNode;
+//        // Examine the stream
+//        var mapping =
+//            (YamlMappingNode)yaml.Documents[0].RootNode;
 
-        foreach (var entry in mapping.Children) {
-            output.WriteLine(((YamlScalarNode)entry.Key).Value);
-        }
+//        foreach (var entry in mapping.Children) {
+//            output.WriteLine(((YamlScalarNode)entry.Key).Value);
+//        }
 
-        // List all the items
-        var items = (YamlSequenceNode)mapping.Children[new YamlScalarNode("items")];
-        foreach (YamlMappingNode item in items) {
-            output.WriteLine(
-                "{0}\t{1}",
-                item.Children[new YamlScalarNode("part_no")],
-                item.Children[new YamlScalarNode("descrip")]
-            );
-        }
-    }
+//        // List all the items
+//        var items = (YamlSequenceNode)mapping.Children[new YamlScalarNode("items")];
+//        foreach (YamlMappingNode item in items) {
+//            output.WriteLine(
+//                "{0}\t{1}",
+//                item.Children[new YamlScalarNode("part_no")],
+//                item.Children[new YamlScalarNode("descrip")]
+//            );
+//        }
+//    }
 
-    private const string Document = @"---
-            receipt:    Oz-Ware Purchase Invoice
-            date:        2007-08-06
-            customer:
-                given:   Dorothy
-                family:  Gale
+//    private const string Document = @"---
+//            receipt:    Oz-Ware Purchase Invoice
+//            date:        2007-08-06
+//            customer:
+//                given:   Dorothy
+//                family:  Gale
 
-            items:
-                - part_no:   A4786
-                  descrip:   Water Bucket (Filled)
-                  price:     1.47
-                  quantity:  4
+//            items:
+//                - part_no:   A4786
+//                  descrip:   Water Bucket (Filled)
+//                  price:     1.47
+//                  quantity:  4
 
-                - part_no:   E1628
-                  descrip:   High Heeled ""Ruby"" Slippers
-                  price:     100.27
-                  quantity:  1
+//                - part_no:   E1628
+//                  descrip:   High Heeled ""Ruby"" Slippers
+//                  price:     100.27
+//                  quantity:  1
 
-            bill-to:  &id001
-                street: |
-                        123 Tornado Alley
-                        Suite 16
-                city:   East Westville
-                state:  KS
+//            bill-to:  &id001
+//                street: |
+//                        123 Tornado Alley
+//                        Suite 16
+//                city:   East Westville
+//                state:  KS
 
-            ship-to:  *id001
+//            ship-to:  *id001
 
-            specialDelivery:  >
-                Follow the Yellow Brick
-                Road to the Emerald City.
-                Pay no attention to the
-                man behind the curtain.
-...";
-
-
+//            specialDelivery:  >
+//                Follow the Yellow Brick
+//                Road to the Emerald City.
+//                Pay no attention to the
+//                man behind the curtain.
+//...";
 
 
 
 
-    // Use this for initialization
-    void Start() {
 
-    }
 
-    // Update is called once per frame
-    void Update() {
+//    // Use this for initialization
+//    void Start() {
 
-    }
-}
+//    }
+
+//    // Update is called once per frame
+//    void Update() {
+
+//    }
+//}
