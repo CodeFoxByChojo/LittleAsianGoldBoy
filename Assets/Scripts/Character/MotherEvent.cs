eb534db54b630dd1a997645a3fbaab3226b4b9c0
Code Fox <playhadde@gmail.com>
2018-01-27 00:23:39 +0100

    created and implemented MotherEvent. Changed inheritance

--- Assets/Scripts/Character/MotherEvent.cs
+++ Assets/Scripts/Character/MotherEvent.cs
@@ -3,17 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using Chojo.LAG.Manager;
 
-
-        // Use this for initialization
-        void Start() {
-
-        }
-
-        // Update is called once per frame
-        void Update() {
 namespace Chojo.LAG.CharacterController {
     public class MotherEvent {
+        private string[] tasknames = { "make the dishes", "trash", "clean up your room", "cook"};
+        private int duration;
+        private string taskname;
+        private static GameManager gameManager = GameManager.getInstance();
 
+        public MotherEvent() {
+            int rand = Random.Range(0, tasknames.Length);
+            taskname = tasknames[rand];
+            duration = Random.Range(gameManager.getConfigData().MinMotherTaskDuration, gameManager.getConfigData().MaxMotherTaskDuration);
         }
     }
 }
