9b8121d6679f1a3a7379004bf8659bbcbc65b312
Code Fox <playhadde@gmail.com>
2018-01-27 00:24:11 +0100

    moved Countable

--- Assets/Scripts/Countables/Countable.cs
+++ Assets/Scripts/Countables/Countable.cs
@@ -1,2 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Chojo.LAG.Manager;
+
 namespace Chojo.LAG.Countable {
     public abstract class CountableClass {
+
+        protected GameManager gameManager;
+        private static CountableClass instance;
+
+        public virtual void oneHourPassed() {
+        }
+
+        protected virtual void attachToHourNotify() {
+            gameManager = GameManager.getInstance();
+            instance = this;
+            gameManager.registerHourNotify(instance);
+            Debug.Log("Attached " + instance + " to hour notify");
+        }
+        protected void attachToHourNotify(CountableClass countable) {
+            gameManager = GameManager.getInstance();
+            gameManager.registerHourNotify(countable);
+            Debug.Log("Attached " + countable + " to hour notify");
+        }
+    }
+}
