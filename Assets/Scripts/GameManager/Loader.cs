6b5b38f480ef1c352ca5e5490db049a6fc450c8e
Code Fox <playhadde@gmail.com>
2018-01-27 00:25:28 +0100

    removed old stuff. removed useless Instantiations

--- Assets/Scripts/GameManager/Loader.cs
+++ Assets/Scripts/GameManager/Loader.cs
@@ -6,53 +6,72 @@ using Chojo.LAG.Environments;
 using Chojo.LAG.CharacterController;
 
 namespace Chojo.LAG.Manager {
+    public class Loader : MonoBehaviour {
 
-    public GameObject configHandler;
-    public GameObject configData;
-    public GameObject dataHandler;
-    public GameObject environment;
-    public GameObject interfaceManager;
-    public GameObject character;
-    public GameObject gameManager;
-    public GameObject gameState;
+        public GameObject configHandler;
+        public GameObject configData;
+        public GameObject dataHandler;
+        public GameObject environment;
+        public GameObject interfaceManager;
+        public GameObject character;
+        public GameObject gameManager;
 
 
 
-    void Awake() {
-        if (GameManager.GameManager.getInstance() == null) {
-            Instantiate(gameManager);
+        void Awake() {
+            if (Environment.getInstance() == null) {
+                Instantiate(environment);
+                while (Environment.getInstance() == null) {
+                    //wait
+                }
+            }
+            if (Character.getInstance() == null) {
+                Instantiate(character);
+                while (Character.getInstance() == null) {
+                    //wait
+                }
+            }
+            if (DataHandler.getInstance() == null) {
+                Instantiate(dataHandler);
+                while (DataHandler.getInstance() == null) {
+                    //wait
+                }
+            }
+            if (ConfigHandler.getInstance() == null) {
+                Instantiate(configHandler);
+                while (ConfigHandler.getInstance() == null) {
+                    //wait
+                }
+            }
+            if (ConfigData.getInstance() == null) {
+                Instantiate(configData);
+                while (ConfigData.getInstance() == null) {
+                    //wait
+                }
+            }
+            if (GameManager.getInstance() == null) {
+                Instantiate(gameManager);
+                while (GameManager.getInstance() == null) {
+                    //wait
+                }
+            }
+            if (UIManager.getInstance() == null) {
+                Instantiate(interfaceManager);
+                while (UIManager.getInstance() == null) {
+                    //wait
+                }
+            }
+
         }
-        if (Environment.Environment.getInstance() == null) {
-            Instantiate(environment);
-        }
-        if (UIManager.getInstance() == null) {
-            Instantiate(interfaceManager);
-        }
-        if (Character.Character.getInstance() == null) {
-            Instantiate(character);
-        }
-        if (GameState.getInstance() == null) {
-            Instantiate(gameState);
-        }
-        if (DataHandler.DataHandler.getInstance() == null) {
-            Instantiate(dataHandler);
-        }
-        if (ConfigHandler.getInstance() == null) {
-            Instantiate(configHandler);
-        }
-        if (ConfigData.getInstance() == null) {
-            Instantiate(configData);
+
+        // Use this for initialization
+        void Start() {
+
         }
 
-}
+        // Update is called once per frame
+        void Update() {
 
-// Use this for initialization
-void Start() {
-
-    }
-
-    // Update is called once per frame
-    void Update() {
-
+        }
     }
 }
