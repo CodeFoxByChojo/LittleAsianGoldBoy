911919e560427bbda27282d81951e0eccdaa81c5
Code Fox <playhadde@gmail.com>
2018-01-27 00:23:05 +0100

    implemented Mother Class and Mother Event

--- Assets/Scripts/Character/Mother.cs
+++ Assets/Scripts/Character/Mother.cs
@@ -4,14 +4,34 @@ using UnityEngine;
 using Chojo.LAG.Countable;
 using Chojo.LAG.Manager;
 
+namespace Chojo.LAG.CharacterController {
+    public class Mother : CountableClass {
 
-        // Use this for initialization
-        void Start() {
+        private int motherTaskWaitDuration;
+        private MotherEvent motherEvent;
+        private static Mother instance;
 
+        private Mother() {
+            attachToHourNotify();
         }
 
-        // Update is called once per frame
-        void Update() {
+        public static Mother getInstance() {
+            if (instance == null) {
+                instance = new Mother();
+            }
+            return instance;
+        }
+
+        protected override void attachToHourNotify() {
+            
+            instance = this;
+            Debug.Log("Instance of Mother is " + instance);
+            Debug.Log("Instance of GameManager is " + gameManager);
+            GameManager.getInstance().registerHourNotify(instance);
+            Debug.Log("Attached " + instance + " to hour notify");
+        }
+
+        private void generateMotherEvent() {
 
         }
     }
