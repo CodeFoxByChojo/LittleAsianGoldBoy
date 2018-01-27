cce85dc0a86cff5a79dffb564a2b01dc9371d326
Code Fox <playhadde@gmail.com>
2018-01-27 23:45:31 +0100

    changes Countable to abstract class

--- Assets/Scripts/Countables/Countable.cs
+++ Assets/Scripts/Countables/Countable.cs
@@ -9,19 +9,13 @@ namespace Chojo.LAG.Countable {
         protected GameManager gameManager;
         private static CountableClass instance;
 
-        public virtual void oneHourPassed() {
-        }
+        public abstract void oneHourPassed();
 
         protected virtual void attachToHourNotify() {
             gameManager = GameManager.getInstance();
             instance = this;
             gameManager.registerHourNotify(instance);
             Debug.Log("Attached " + instance + " to hour notify");
-        }
-        protected void attachToHourNotify(CountableClass countable) {
-            gameManager = GameManager.getInstance();
-            gameManager.registerHourNotify(countable);
-            Debug.Log("Attached " + countable + " to hour notify");
         }
     }
 }
