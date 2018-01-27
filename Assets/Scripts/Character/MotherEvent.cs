5bb1b5b4c2ac3c2839d52285e97bf8da965a4c61
Code Fox <playhadde@gmail.com>
2018-01-27 23:44:51 +0100

    implemented MotherEvent class. Generates on instantiation a random Event.

--- Assets/Scripts/Character/MotherEvent.cs
+++ Assets/Scripts/Character/MotherEvent.cs
@@ -5,15 +5,33 @@ using Chojo.LAG.Manager;
 
 namespace Chojo.LAG.CharacterController {
     public class MotherEvent {
-        private string[] tasknames = { "make the dishes", "trash", "clean up your room", "cook"};
+        private string[] tasknames = { "make the dishes", "trash", "clean up your room", "cook" };
         private int duration;
+        private int waitDuration;
         private string taskname;
+        private bool eventActive = false;
         private static GameManager gameManager = GameManager.getInstance();
 
         public MotherEvent() {
             int rand = Random.Range(0, tasknames.Length);
             taskname = tasknames[rand];
             duration = Random.Range(gameManager.getConfigData().MinMotherTaskDuration, gameManager.getConfigData().MaxMotherTaskDuration);
+            waitDuration = Random.Range(gameManager.getConfigData().MinMotherWaitDuration, gameManager.getConfigData().MaxMotherWaitDuration);
+        }
+
+        public int getWaitDuration() {
+            return waitDuration;
+        }
+        public int getDuration() {
+            return duration;
+        }
+        public bool isEventActive() {
+            return eventActive;
+        }
+
+        //Methode zum herabsetzen der duration.
+        public void oneHourPassed() {
+            duration = duration - 1;
         }
     }
 }
