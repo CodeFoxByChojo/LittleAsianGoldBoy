da39fe2e97e2ae53a3dddef53d9d4608c1de5cc8
Code Fox <playhadde@gmail.com>
2018-01-28 21:29:47 +0100

    added options for computer upgrade

--- Assets/Scripts/GameManager/ButtonHandler.cs
+++ Assets/Scripts/GameManager/ButtonHandler.cs
@@ -9,29 +9,30 @@ namespace Chojo.LAG.Manager {
 
         public Button button;
 
-        public defines.ButtonIdentiy buttonIdentity;
-        public defines.ButtonType buttonType;
+        public Defines.ButtonIdentiy buttonIdentity;
+        public Defines.ButtonType buttonType;
         public int ComputerID;
-        private UIManager uiManager = UIManager.getInstance();
+        private UIManager uiManager;
 
 
-        // Use this for initialization
         void Start() {
+            uiManager = UIManager.GetInstance();
             Button btn = button.GetComponent<Button>();
             btn.onClick.AddListener(TaskOnClick);
         }
 
         private void TaskOnClick() {
-            Debug.Log("Button Clicked");
+            Debug.Log("Button Clicked. Parameter: Identity: " + buttonIdentity + " Type: " + buttonType);
+            if (buttonIdentity != Defines.ButtonIdentiy.Computer) {
+                uiManager.ButtonClickedEvent(buttonIdentity, buttonType);
+            } else {
+                uiManager.ButtonClickedEvent(buttonIdentity, buttonType, ComputerID);
+            }
         }
 
         // Update is called once per frame
         void Update() {
 
-        }
-
-        private void OnMouseDown() {
-            //uiManager.ButtonClickedEvent(buttonIdentity, buttonType);
         }
     }
 }
