02a9f443ab9dd8abd701811dbb5044bd43b13046
Code Fox <playhadde@gmail.com>
2018-01-28 21:31:16 +0100

    added notifier, who notifies the gameManager about the hour cycle

---
+++ Assets/Scripts/GameManager/Notifier.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Chojo.LAG.Manager;
+using Chojo.LAG.DataController;
+
+public class Notifier : MonoBehaviour {
+
+    private float oneTimeCycleInSeconds;
+    private float timeToNextCycle = 0;
+
+    private static DataHandler dataHandler = DataHandler.getInstance();
+    private ConfigData configData = dataHandler.getConfigData();
+    private static GameManager gameManager = GameManager.GetInstance();
+
+
+    void Start() {
+        oneTimeCycleInSeconds = configData.TimeCycleInSeconds;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        timeToNextCycle += Time.deltaTime;
+        if (timeToNextCycle > oneTimeCycleInSeconds) {
+            gameManager.HourPassed();
+            timeToNextCycle = 0;
+        }
+    }
+}
